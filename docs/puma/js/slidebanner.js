3dde7926914417feabb816d48d3916225b43fecf
yuyuring <ayase01@naver.com>
2021-01-10 21:13:37 +0900

    add puma
    add puma

---
+++ docs/puma/js/slidebanner.js
@@ -0,0 +1,53 @@
+$(function(){ 
+    var visual = $('.brandVisual > ul > li');
+    var button = $('.buttonList > li');
+    var current = 0;
+    var num =visual.length;
+    var setIntervalId;
+    
+    button.on({
+        click:function(){
+            var tg = $(this);
+            var i = tg.index();
+
+            button.removeClass('on');
+            tg.addClass('on');
+
+            move(i);
+        }
+    });
+    
+    $('.brandVisual, .buttonList').on({
+        mouseover:function(){
+            clearInterval(setIntervalId);
+        },
+        mouseout:function(){
+            timer();
+        }
+    });
+    
+    timer();
+    function timer(){
+        setIntervalId = setInterval(function(){
+            var n = current + 1;
+            if(n == num){
+                n = 0;
+            }
+            
+            button.eq(n).trigger('click');
+        }, 3000);
+        
+    }
+    
+    function move(i){
+        if(current == i) return;
+        
+        var currentEl = visual.eq(current);
+        var nextEl = visual.eq(i);
+        
+        currentEl.css({left:0}).stop().animate({left:'-100%'});
+        nextEl.css({left:'100%'}).stop().animate({left:0});
+
+        current = i;
+    }
+});
